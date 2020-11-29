f1028744ba572e22858c4fc24546d097e0a0b79f
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 21:50:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,11 +4,21 @@ while count < 5 do
   puts message
   count += 1
 end
+end
 
 def loop_message_n_times(message, number)
   count = 0
   while count < number do
     puts message
+    count += 1
+  end
+end
+
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
     count += 1
   end
 end
